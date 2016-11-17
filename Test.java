a4f88b023f84ef6a7fe1b9b250b2b8eb94df8930
vrana22 <rana.abes@gmail.com>
2016-11-17 10:33:52 +0000

    Update Test.java
    Commit3

--- Test.java
+++ Test.java
@@ -1,6 +1,6 @@
 import static java.lang.System.*;
 public class TestProg {
 	public static void main(String[] args) {
-		System.out.println("Hello Vikas");
+		System.out.println("Hello Vikas Rana");
 	}
 }
